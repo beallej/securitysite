d3d28fe8fc3e6c26fbf1c6a1725c27a2c8c88a0f
Chet Aldrich <chetaldrich@gmail.com>
2014-11-06 14:21:50 -0600

    Added sources.html

--- pages/phptest.php
+++ pages/phptest.php
@@ -1,6 +1,8 @@
 <?php
 $output = shell_exec('curl https://shellshocker.net/shellshock_test.sh | bash');
-$formattedOutput = str_replace("not vulnerable", "<font color=\"green\"> not vulnerable </font>", $output);
-$formattedOutput2 = str_replace("[39m", " ", $formattedOutput);
-echo "<pre>$formattedOutput2</pre>";
+$formattedOutput0 = str_replace("not vulnerable", "<font color=\"green\">not vulnerable</font>", $output);
+$formattedOutput = str_replace("VULERABLE", "<font color = \"red\"> VULNERABLE</font>", $formattedOutput0);
+$formattedOutput1 = str_replace("[92m", " ", $formattedOutput0);
+$formattedOutput2 = str_replace("[39m", " ", $formattedOutput1);
+echo "<pre>$formattedOutput2</pre><br><br>If you have any <font color = \"red\"> VULNERABLE</font>'s, your bash is vulnerable.";
 ?>
